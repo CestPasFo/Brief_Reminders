4dda4deed7c6aabb389b469c9e4114685126efe2
CestPasFo <c.jaworski12@hotmail.fr>
2024-11-27 11:52:09 +0100

    Feat : ajout de la config pour tailwind + form

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.{html.twig,js}"],
+  content: ["./templates/.{html,html.twig,js}"],
   theme: {
     extend: {},
   },
