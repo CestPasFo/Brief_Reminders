5809ba09bbf48f516abb746d0d4b23cbda8fad4e
CestPasFo <c.jaworski12@hotmail.fr>
2024-11-27 10:13:26 +0100

    Feat: Création du CRUD Category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,18 +2,58 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CategoryController extends AbstractController
 {
-    #[Route('/category', name: 'app_category')]
-    public function index(): JsonResponse
+    #[Route('/api/categories', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/CategoryController.php',
-        ]);
+        $category = $serializer->deserialize($request->getContent(), Category::class, 'json');
+        $entityManager->persist($category);
+        $entityManager->flush();
+        
+        return $this->json($category, Response::HTTP_CREATED);
+    }
+
+    #[Route('/api/categories', methods: ['GET'])]
+    public function index(EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    {
+        $categories = $entityManager->getRepository(Category::class)->findAll();
+        $json = $serializer->serialize($categories, 'json');
+        
+        return new JsonResponse($json, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/categories/{id}', methods: ['GET'])]
+    public function show(Category $category, SerializerInterface $serializer): JsonResponse
+    {
+        $json = $serializer->serialize($category, 'json');
+        return new JsonResponse($json, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/categories/{id}', methods: ['PUT'])]
+    public function update(Request $request, Category $category, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    {
+        $serializer->deserialize($request->getContent(), Category::class, 'json', ['object_to_populate' => $category]);
+        $entityManager->flush();
+        
+        return $this->json($category);
+    }
+
+    #[Route('/api/categories/{id}', methods: ['DELETE'])]
+    public function delete(Category $category, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $entityManager->remove($category);
+        $entityManager->flush();
+        
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
